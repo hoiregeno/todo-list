0daf00694a91abfc5a0f67889e8b184645c59881
hoiregeno <hoiregeno75@gmail.com>
2025-07-24 11:23:11 +1000

    chore(script.js): adjust indent in functions for readability

--- script.js
+++ script.js
@@ -9,53 +9,53 @@ listContainer.addEventListener("contextmenu", deleteTask); // Listens for right 
 
 // Adds a new task to the list.
 function addTask(event){
-if(event.key === "Enter") {
-  if(inputBox.value === "") {
-    alert("Please enter something.");	// displays an error message if user enters nothing.
-    return; // Exit function. Do nothing.
+  if(event.key === "Enter") {
+    if(inputBox.value === "") {
+      alert("Please enter something.");	// displays an error message if user enters nothing.
+      return; // Exit function. Do nothing.
+    }
+
+    let li = document.createElement("li"); // if user entered something, create an 'li' element to the DOM.
+    li.innerHTML = inputBox.value;         // set the text content to whatever the user entered.
+    listContainer.appendChild(li);	       // adds the li element to the listContainer.
+
+    inputBox.value = "";	// resets the input field.
+    saveTask();	// update the list after task is entered.
   }
-
-  let li = document.createElement("li"); // if user entered something, create an 'li' element to the DOM.
-  li.innerHTML = inputBox.value;         // set the text content to whatever the user entered.
-  listContainer.appendChild(li);	       // adds the li element to the listContainer.
-
-  inputBox.value = "";	// resets the input field.
-  saveTask();	// update the list after task is entered.
-}
 }
 
 // Marks a task as complete when user left clicks mouse.
 function checkTask(event){
-const task = event.target; // Grab the task and store it in a variable so we can reuse it.
+  const task = event.target; // Grab the task and store it in a variable so we can reuse it.
 
-if(task.tagName === "LI") {
-  task.classList.toggle("checked");
-  saveTask();	// update the list when task is marked.
-}
+  if(task.tagName === "LI") {
+    task.classList.toggle("checked");
+    saveTask();	// update the list when task is marked.
+  }
 }
 
 // Deletes a task when user right clicks mouse.
 function deleteTask(event){
-event.preventDefault(); // Prevents context menu from popping up.
+  event.preventDefault(); // Prevents context menu from popping up.
 
-const task = event.target; // Grab the task and store it in a variable so we can reuse it.
-if(task.tagName === "LI") {
-  task.remove(); // if the tag name is LI. Remove it from the DOM.
-  saveTask();    // update the list when task is deleted.
+  const task = event.target; // Grab the task and store it in a variable so we can reuse it.
+  if(task.tagName === "LI") {
+    task.remove(); // if the tag name is LI. Remove it from the DOM.
+    saveTask();    // update the list when task is deleted.
 }
 }
 
 // Saves the list to local storage. Helps us update the list.
 function saveTask() {
-localStorage.setItem("tasks", listContainer.innerHTML);
+  localStorage.setItem("tasks", listContainer.innerHTML);
 }
 
 // Loads the list to the browser.
 function loadTask() {
-const tasks = localStorage.getItem("tasks");
-if(tasks){
-  listContainer.innerHTML = tasks;
-}
+  const tasks = localStorage.getItem("tasks");
+  if(tasks){
+    listContainer.innerHTML = tasks;
+  }
 }
 
 loadTask();	// Load the updated list right away.
